4d510a91376557b47fcf5f75f1f981352ea3b76c
MohamedJamaal <mohamed_gamal19@hotmail.com>
2020-12-17 03:49:03 +0200

    Add routes to dashboard, signup and login pages

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Signup from './Signup';
 import {Container} from 'react-bootstrap'
 import { AuthProvider } from '../contexts/AuthContext';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Dashboard from './Dashboard'
+import Login from './Login'
 
 function App() {
   return (
@@ -11,7 +14,15 @@ function App() {
         style={{ minHieght: '100vh' }}
       >
         <div className="w-100" style={{ maxWidth: '400px' }} >
-          <Signup />
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <Route exact path="/" component={Dashboard} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+              </Switch>
+            </AuthProvider>
+          </Router>
         </div>
       </Container>
     </AuthProvider>
